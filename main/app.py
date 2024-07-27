46ff9231e616a258b6bccbb070b829ebb9e4620c
20231112 <150868919+20231112@users.noreply.github.com>
2024-07-27 17:04:27 +0900

    Add files via upload

---
+++ main/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, send_file
+import os
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        return render_template('index.html')
+    else:
+        InputFile = request.files['file']
+        InputFile.save(os.path.join("uploads", InputFile.filename))
+        
+        data=open(os.path.join("uploads", InputFile.filename)).read()
+        data=data+"apend"
+        with open(os.path.join("downloads", "downloads.txt"), mode="w") as f:
+            f.write(data)
+        
+        return send_file(os.path.join("downloads", "downloads.txt"),attachment_filename="downloads.txt",as_attachment=True)
+
+
+if __name__ == '__main__':
+    app.run(port=5001)
